34f01d7fc4cac63defc21940c913b54b03e6778f
Ingrid Soto <ingridsoto.prog@gmail.com>
2022-03-11 16:09:36 -0300

    Autenticação com GitHub

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,23 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, useSession, signOut } from "next-auth/react";
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ?  (
-    <button className={styles.signInButton} type="button">
+  return session ? (
+    <button className={styles.signInButton} type="button" onClick={() => signOut()}>
       <FaGithub color="#04D361" />
-      Ingrid Soto
-      <FiX color="#737380" className={styles.closeIcon}/>
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
     </button>
-
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#EBA417" />
       Sign in with Github
     </button>
