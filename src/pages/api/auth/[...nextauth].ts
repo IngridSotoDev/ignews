6ac00225e8c8a32fc0dc14f575aa6cb53f94294b
Ingrid Soto <ingridsoto.prog@gmail.com>
2022-03-14 09:01:09 -0300

    Configuração FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query as q } from "faunadb";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -8,9 +11,32 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
       authorization: {
         params: {
-          scope: 'read:user'
-        }
-      }
+          scope: "read:user",
+        },
+      },
     }),
   ],
-})
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const Email = user.email;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email: Email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
